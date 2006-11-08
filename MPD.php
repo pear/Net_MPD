efa998bc8346755c568f14614f2c4a0b443c8652
Graham Christensen <itrebal@php.net>
2006-11-08 00:34:11 +0000

    Preparing for 0.2.0-dev release, changes no Net_MPD and Net_MPD_Common
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_MPD/trunk@222976 c90b9560-bf6c-de11-be94-00142212c4b1

--- MPD.php
+++ MPD.php
@@ -26,7 +26,7 @@ require_once 'MPD/Common.php';
 
 
 /**
- * API for the factory portion of Music Player Daemon commands
+ * Central class for the Music Player Daemon objects
  *
  * Used to utilize the functionality of the provided classes
  * 
@@ -42,6 +42,48 @@ require_once 'MPD/Common.php';
 class Net_MPD
 {
     /**
+     * The Net_MPD_Admin object
+     */
+    public $Admin;
+
+    /**
+     * The Net_MPD_Common object
+     */
+    public $Common;
+
+    /**
+     * The Net_MPD_Database object
+     */
+    public $Database;
+
+    /**
+     * The Net_MPD_Playback object
+     */
+    public $Playback;
+
+    /**
+     * The Net_MPD_Playlist object
+     */
+    public $Playlist;
+    
+    /**
+     * Creates new instances of objects
+     * @param $host Host to connect to, optional (default: localhost)
+     * @param $port Port to connect to, optional (default: 6600)
+     * @param $pass Pass to connect to, optional (default: null)
+     * @return object or false on failure
+     */
+    function __construct($host = 'localhost', $port = 6600, $pass = null)
+        {
+            
+            $this->Admin    = self::factory('Admin', $host, $port, $pass);
+            $this->Common   = self::factory('Common', $host, $port, $pass);
+            $this->Database = self::factory('Database', $host, $port, $pass);
+            $this->Playback = self::factory('Playback', $host, $port, $pass);
+            $this->Playlist = self::factory('Playlist', $host, $port, $pass);
+        }
+    
+    /**
      * Creates new instances of objects
      * @param $class Class to initiate, with out Net_MPD_$class
      * @param $host Host to connect to, optional (default: localhost)
@@ -56,13 +98,7 @@ class Net_MPD
             }
         
             $class = 'Net_MPD_'.$class;
-            $obj = new $class();
-        
-            try {
-                $obj->connect($host, $port, $pass);
-            } catch (PEAR_Exception $e) {
-                throw new PEAR_Exception($e->getMessage(), $e);
-            }
+            $obj = new $class($host, $port, $pass);
         
             return $obj;
         }
