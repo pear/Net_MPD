b655f80b85b943d5c72e0caeebc815e05e61df88
Graham Christensen <itrebal@php.net>
2006-11-08 00:37:29 +0000

    Added files I forgot to add previously
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_MPD/trunk@222977 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ MPD/docs/listSongs.php
@@ -0,0 +1,14 @@
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL | E_STRICT);
+require_once 'MPD.php';
+$MPD_DB = Net_MPD::factory('Database');
+
+
+//Search for songs by the Artist "Modest Mouse"
+$dump = $MPD_DB->find(array('Artist' => 'Goldfinger'));
+echo '<ol>';
+foreach ($dump as $song) {
+    echo '<li>'.$song['Title'].'</li>';
+}
+echo '</ol>';
