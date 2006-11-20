846cdbd05db14c7b56e92d36caa44643807d8b4d
Graham Christensen <itrebal@php.net>
2006-11-20 22:13:53 +0000

    Fixing bug 9334; Moving files to Net/
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_MPD/trunk@223633 c90b9560-bf6c-de11-be94-00142212c4b1

--- MPD/docs/listCurrentPlaylist.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'MPD.php';
-$MPD_PLS = Net_MPD::factory('Playlist');
-if (!$MPD_PLS->connect()) {
-    die('Connection failed: '.print_r($MPD_DB->getErrors(), true));
-}
-
-$data = $MPD_PLS->getPlaylistInfo();
-var_dump($data);
